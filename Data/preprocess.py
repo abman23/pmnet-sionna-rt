52fde5b1bcef36bea412a5aecf1059b7cec0bf6e
Arjun Balamwar <arjunbalamwar@gmail.com>
2024-04-15 17:37:57 -0700

    Updated to version 3

--- Data/preprocess.py
+++ Data/preprocess.py
@@ -54,7 +54,7 @@ def augment_image(image):
 
   return image, flipped_vertical, flipped_horizontal, flipped_both
 
-def crop_image_with_tx(root, bs, tx_image, building_mask, power, point, w, crop_dim=np.array((256, 256)), stride=1):
+def crop_image_with_tx(root, bs, tx_image, building_mask, power, point, w, crop_dim=np.array((512, 512)), stride=80):
     os.makedirs(ROOT+f"/cropped/tx_map", exist_ok=True)
     os.makedirs(ROOT+f"/cropped/power_map", exist_ok=True)
     os.makedirs(ROOT+f"/cropped/city_map", exist_ok=True)
@@ -73,8 +73,16 @@ def crop_image_with_tx(root, bs, tx_image, building_mask, power, point, w, crop_
         for x in range(0, width - crop_width + 1, stride_x):
             # Check if the point is present in the current cropped region
             if x <= min_sq[0] < x + crop_width and x <= max_sq[0] < x + crop_width and y <= min_sq[1] < y + crop_height and y <= max_sq[1] < y + crop_height:
-                crop_tx, crop_bld, crop_p = augment_image(tx_image[y:y + crop_height, x:x + crop_width]), augment_image(building_mask[y:y + crop_height, x:x + crop_width]), augment_image(power[y:y + crop_height, x:x + crop_width])
-
+                
+                crop_tx = augment_image(
+                    cv2.resize(tx_image[y:y + crop_height, x:x + crop_width], (256, 256))
+                )
+                crop_bld = augment_image(
+                    cv2.resize(building_mask[y:y + crop_height, x:x + crop_width], (256, 256))
+                )
+                crop_p = augment_image(
+                    cv2.resize(power[y:y + crop_height, x:x + crop_width], (256, 256))
+                )
 
                 cv2.imwrite(root+f"cropped/tx_map/{bs}_{count}_0.png", crop_tx[0])
                 cv2.imwrite(root+f"cropped/tx_map/{bs}_{count}_1.png", crop_tx[1])
@@ -96,59 +104,66 @@ def crop_image_with_tx(root, bs, tx_image, building_mask, power, point, w, crop_
 
 
 if __name__ == "__main__":
-  ROOT = "Data/"
-  TX =  np.load(f"{ROOT}tx_positions.npy")
-  CROP_DIM = np.array((256, 256))
-  STRIDE = 65
-  TX_WIDTH = 8
-  FLOOR = -200
-  scene = get_scene(ROOT + "USC_3D/USC.xml")
+    ROOT = "data/"
+    TX =  np.load(f"{ROOT}tx_positions.npy")
+    CROP_DIM = np.array((512, 512))
+    STRIDE = 80
+    TX_WIDTH = 8
+    FLOOR = -200
+    scene = get_scene(ROOT + "USC_3D/USC.xml")
 
-  cm_dim = 900
+    cm_dim = 900
 
-  city_map = cv2.imread(f"{ROOT}city_map_usc.png")[:, :, 0]
-  city_map = cv2.resize(city_map, (cm_dim, cm_dim))
-  city_dim = city_map.shape[0]
+    city_map = cv2.imread(f"{ROOT}USC_city_map.png")[:, :, 0]
 
-  start, end = int(sys.argv[1]), int(sys.argv[2]) 
-  batch_size = end-start
+    start, end = int(sys.argv[1]), int(sys.argv[2]) 
+    batch_size = end-start
 
-  if end > len(TX):
-     print(f"Please enter an end less than number of TX points ({len(TX)})")
-  elif start < 0:
-     print(f"start should be greater than 0")
-  elif batch_size <= 0:
-     print(f"end should be greater than start")
-  else:
-    for i in range(start, end):
-      tx = Transmitter(f"tx{i}", TX[i]+[0, 0, 2], [0.0, 0.0, 0.0])
-      scene.add(tx)
+    if end > len(TX):
+        print(f"Please enter an end less than number of TX points ({len(TX)})")
+    elif start < 0:
+        print(f"start should be greater than 0")
+    elif batch_size <= 0:
+        print(f"end should be greater than start")
+    else:
+        for i in range(start, end):
+            tx = Transmitter(f"tx{i}", TX[i]+[0, 0, 2], [0.0, 0.0, 0.0])
+            scene.add(tx)
 
-    cm = scene.coverage_map(
-        cm_cell_size=(5.0, 5.0),
-        diffraction=True, scattering=True, edge_diffraction=True, max_depth = 1000, num_samples = 8.99*(10**6)
-    )
-    for i in range(start, end):
-        tx_cm = 10.*np.log10(cm[i%batch_size].numpy())
-        tx_cm[tx_cm==(-np.inf)] = -250
-        tx_cm = cv2.resize(tx_cm, city_map.shape)
+        cm = scene.coverage_map(
+            cm_cell_size=(5.0, 5.0),
+            diffraction=True, scattering=True, edge_diffraction=True, max_depth = 1000, num_samples = 8.99*(10**6)
+        )
+        for i in range(start, end):
+            tx_cm = 10.*np.log10(cm[i%batch_size].numpy())
+            tx_cm[tx_cm==(-np.inf)] = -255
+            tx_cm[tx_cm > 0] = 0
+            tx_cm = cv2.resize(tx_cm, city_map.shape)
 
-        tx_cm = cv2.flip(tx_cm, 0)
-        tx_cm[tx_cm < FLOOR] = FLOOR
-        tx_cm[city_map == 0] = -250
-        tx_cm += 250
+            tx_cm = cv2.flip(tx_cm, 0)
+            tx_cm[tx_cm < FLOOR] = FLOOR
+            tx_cm += 255
 
-        tx_pos= np.array(TX[i])+(cm_dim//2) + (50)
-        tx_pos = (tx_pos*city_dim/cm_dim).astype(np.int16)
+            tx_cm[city_map < 55] = 0 # for building
 
-        tx_map = np.zeros((city_dim, city_dim))
-        shift = TX_WIDTH//2
-        tx_map[city_dim - tx_pos[1]-shift:city_dim - tx_pos[1] +shift, tx_pos[0]-shift:tx_pos[0]+shift] = 255
+            if tx_cm.min() != 0: continue
 
-        uncropped =tx_cm+tx_map
+            tx_pos= np.array(TX[i])+(cm_dim//2) + (50)
+            tx_pos = (tx_pos).astype(np.int16)
 
-        os.makedirs(ROOT+f"/uncropped", exist_ok=True)
-        cv2.imwrite(ROOT+f"/uncropped/{i}.png", uncropped)
-        crop_image_with_tx(ROOT, f"{i}", tx_map, city_map, tx_cm, tx_pos[:2], w = TX_WIDTH, crop_dim=CROP_DIM, stride=STRIDE)
+            tx_map = np.zeros((cm_dim, cm_dim))
+            shift = TX_WIDTH//2
+            tx_map[cm_dim - tx_pos[1]-shift:cm_dim - tx_pos[1] +shift, tx_pos[0]-shift:tx_pos[0]+shift] = 255
 
-  print("Completed")
+            uncropped =tx_cm.copy()
+            bld_filt = tx_cm == 0
+            tx_filt = tx_map == 255
+            uncropped[bld_filt] = city_map[bld_filt]
+            uncropped[tx_filt] = 255
+            uncropped = np.clip(uncropped.astype(np.uint8), 0, 255)
+
+            os.makedirs(ROOT+f"/uncropped", exist_ok=True)
+            cv2.imwrite(ROOT+f"/uncropped/{i}.png", uncropped)
+            crop_image_with_tx(ROOT, f"{i}", tx_map, city_map, tx_cm, tx_pos[:2], w = TX_WIDTH, crop_dim=CROP_DIM, stride=STRIDE)
+
+    print(f"Completed for tx {start} - {end}")
